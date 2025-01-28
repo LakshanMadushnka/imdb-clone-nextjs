5802f3cd73f0efcb502135e11344ebf964f0df85
LakshanMadushnka <lranasinghe286@gmail.com>
2025-01-28 15:11:47 +0530

    Add mongodb to connect clerk and the database

---
+++ src/lib/mongodb/mongoose.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+let initialized = false;
+
+export const connect = async () => {
+    mongoose.set('strictQuery', true);
+    if (initialized) {
+        console.log('Already connected to MongoDB');
+        return;
+    }
+    try {
+        await mongoose.connect(process.env.MONGODB_URI, {
+            dbName: 'next-imdb-clerk',
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        initialized = true;
+        console.log('Connected to MongoDB');
+    }catch(error){
+        console.log('Error connecting to MongoDB:', error);
+    }
+};
